59f2e979e9d6b0f3fc55e43265592b7f935e7ffc
ZRNJJ <pacheco.kingjayjay@minsu.edu.ph>
2025-04-02 08:38:54 +0800

    APRIL 2, 2025

--- LTS_app/Controllers/HomeController.cs
+++ LTS_app/Controllers/HomeController.cs
@@ -47,6 +47,12 @@ namespace LTS_app.Controllers
             return View();
         }
 
+
+        public IActionResult UnderConstruction()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- LTS_app/obj/Release/net9.0/LTS_app.AssemblyInfo.cs
+++ LTS_app/obj/Release/net9.0/LTS_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LTS_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa3927acf444b2ea2db382f3520d27afb1c67937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac40742a37d12bc6604cea3115f753f8224a805")]
 [assembly: System.Reflection.AssemblyProductAttribute("LTS_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LTS_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
